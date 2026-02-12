0ae2ef5b34c08c27f37b5fe664e696abab314ff1
Aymane Boujeddi <aymane655@gmail.com>
2026-02-12 11:31:14 +0100

    feat: add loader component and update navbar for authentication state handling

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  protected authService = inject(AuthService);
+
+  onLogout(): void {
+    this.authService.logout();
+  }
+}
