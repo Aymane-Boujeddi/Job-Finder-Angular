6c2e10c448af17197b01804850d7e78d68e5d695
Aymane Boujeddi <aymane655@gmail.com>
2026-02-12 11:36:02 +0100

    feat: refactor app configuration for improved readability and organization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { favoritesReducer } from './features/favorites/store/favorite.reducer';
+import { FavoriteEffects } from './features/favorites/store/favorite.effects';
+import { apiInterceptor } from './core/interceptors/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor , apiInterceptor])),
+    provideStore({ favorites: favoritesReducer }),
+    provideEffects([FavoriteEffects]),
+    provideStoreDevtools({ maxAge: 25 }),
+  ],
 };
