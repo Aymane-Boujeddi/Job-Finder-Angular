2bd15ee627ef175ac2d272629ca9d304256b56e3
Aymane Boujeddi <aymane655@gmail.com>
2026-02-14 20:57:13 +0100

    feat: add application management data to db.json and create application routes

---
+++ src/app/features/applications/applications.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { ApplicationListPageComponent } from './pages/application-list-page/application-list-page.component';
+
+export const applicationsRoutes: Routes = [{ path: '', component: ApplicationListPageComponent }];
