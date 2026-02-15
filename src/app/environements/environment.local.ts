f42260c8b96213315234bd79784fde60e5ce1556
Aymane Boujeddi <aymane655@gmail.com>
2026-02-15 12:54:03 +0000

    refactor: remove local environment configuration file

--- src/app/environements/environment.local.ts
+++
@@ -1,6 +0,0 @@
-export const  environement = {
-    apiKey : 'Mq5H0k1W6dQaSryUrNBWsLHn7/GXxF0kdAPLdBUil4I=',
-    apiUrl : "https://data.usajobs.gov/api/Search",
-    apiSource : "data.usajobs.gov/api",
-    // email : 'hordyjones007@gmail.com'
-};
