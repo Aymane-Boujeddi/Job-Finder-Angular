86af36279f986413933ff25262bb64dcd969d394
Aymane Boujeddi <aymane655@gmail.com>
2026-02-15 12:52:53 +0000

    feat: add favoritesOffers to db.json and create environment configuration file

---
+++ src/app/core/environements/environment.local.ts
@@ -0,0 +1,6 @@
+export const  environement = {
+    apiKey : 'Mq5H0k1W6dQaSryUrNBWsLHn7/GXxF0kdAPLdBUil4I=',
+    apiUrl : "https://data.usajobs.gov/api/Search",
+    apiSource : "data.usajobs.gov/api",
+    // email : 'hordyjones007@gmail.com'
+};
